844852f056b2929a31fd9427a9880df7490373a0
rorogarcete <oneosmar89@gmail.com>
2017-09-19 23:52:31 -0400

    Initial Commit

---
+++ JoinEvents/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JoinEvents
+//
+//  Created by Rodrigo Garcete on 19/9/17.
+//  Copyright © 2017 GDG CDE. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
